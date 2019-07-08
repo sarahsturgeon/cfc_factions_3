9c341f77b4dab720883ec725c981ff5af651ab98
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-07-08 14:08:05 -0700

    Refactor sv_player_ext.lua

--- lua/cfcfactions/core/sv/sv_player_ext.lua
+++ lua/cfcfactions/core/sv/sv_player_ext.lua
@@ -1,8 +1,8 @@
---[[]
+--[[
+
 File Name: sv_player_ext.lua
 
 Purpose: Player table data that contain generic server-side only functions and not shared.
-
 
 ]]--
 
@@ -15,54 +15,59 @@ function meta:CFCToggleMenu()
 end
 
 --[[
-    TODO: rework a lot of this into sv_users.lua
+TODO: rework a lot of this into sv_users.lua
 ]]--
 
 --Checks to see if a player is currently in a faction. returns false if not in one.
 function meta:IsInFaction()
-    if self:IsMerc() == true then return true end
-    
+    if self:IsMerc() then return false end
+
     local userHasFaction = self:GetFactionID() ~= nil
+
     return userHasFaction
 end
 
---Obtains the player's faction id, or 0 if not. Bots always return 'b0t'
+--Obtains the player's faction id, or 0 if not. nil if invalid or not found.
 function meta:GetFactionID()
-    if self:IsBot() then return "b0t" end
-    if self:IsPlayer() then
-       return fpm[self:SteamID64()] and fpm[self:SteamID64()].FactionID
-    end
+    if not self:IsPlayer() then return end
+
+    local steam64 = self:SteamID64()
+
+    local playerFPM = fpm[steam64]
+
+    if playerFPM then return playerFPM end
 end
 
---Obtains the player's rank if in a faction. Returns empty string if not
+--Obtains the player's rank if in a faction. Returns nil otherwise.
 function meta:GetFactionRank()
-    if self:IsInFaction() then 
-        return fpm[self:SteamID64()].FactionRank 
-    else
-        return ""
-    end
+    if not self:IsInFaction() then return end
+
+    local playerFPM = fpm[self:SteamID64()]
+    local factionRank = playerFPM.FactionRank
+
+    return factionRank
 end
 
 --Gets a player's faction as a table. Returns an empty table if not in one
 function meta:GetFaction()
-    if self:IsInFaction() then
-        if cfcFactions.Factions[self:GetFactionID()] ~= 0 then
-            return cfcFactions.Factions[self:GetFactionID()]
-        end
-    else
-        return {}
-    end
+    if not self:IsInFaction() then return end
+
+    local playerFactionID = self:GetFactionID()
+    local playerFaction = cfcFactions.Factions[playerFactionID]
+
+    local playerFactionIsInvalid = playerFaction == 0
+    if playerFactionIsInvalid then return end
+
+    return playerFaction
 end
 
 --Sets a player's faction based on given id. ply being who is doing the setting
 function meta:SetFactionID(id)
     fpm.Users[self:SteamID64()].FactionID = id
-
 end
 
 function meta:SetFactionRank(rank)
     fpm.Users[self:SteamID64()].FactionRank = rank
-
 end
 
 function meta:IsMerc()
