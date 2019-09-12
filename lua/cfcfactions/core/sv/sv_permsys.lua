182e493ee47bd76627cf12d3d6a34353f7b678db
mw-voo <necro343@gmail.com>
2019-09-12 12:25:53 -0500

    Cleaning up permission system
    Removal of permission aliases

--- lua/cfcfactions/core/sv/sv_permsys.lua
+++ lua/cfcfactions/core/sv/sv_permsys.lua
@@ -13,68 +13,63 @@ fpm.Users = {}
 
 fpm.Permissions.CorePermissions = {
         --  NamedKey = table(description, all_caps_string_followed_by_underscores_for_spaces)
-        --"ExamplePermission" = {Description="A short description of what the permission should do", Alias="EXAMPLE_PERM"}
-        --"CanKick" = {Description="Allows a user to kick from faction.", Alias="CAN_KICK"}
-        --[""] = {Description="", Alias=""},
+        --"ExamplePermission" = {Description="A short description of what the permission should do"}
+        --"CanKick" = {Description="Allows a user to kick from faction."}
+        --[""] = {Description=""},
 
         --faction managment
-        ["CanBan"] = {Description="Allows the user to ban from their own faction.", Alias="CAN_BAN"},
-        ["CanUnban"] = {Description="Allows the user to unban a member from their own faction.", Alias="CAN_UNBAN"},
-        ["CanDisbandFaction"] = {Description="Allows a user to disband their faction.", Alias="CAN_DISBAND_FACTION"},
-        ["CanEditAll"] = {Description="Allows a user to edit any faction detail.", Alias="CAN_EDIT_ALL"},
-        ["CanEditDescription"] = {Description="Allows a user to edit the faction's description.", Alias="CAN_EDIT_DESCRIPTION"},
-        ["CanEditName"] = {Description="Allows a user to edit the faction's name.", Alias="CAN_EDIT_NAME"},
-        ["CanEditColor"] = {Description="Allows a user to edit the faction's color.", Alias="CAN_EDIT_COLOR"},
-        ["CanEditInvite"] = {Description="Allows a user to edit the faction's invite status.", Alias="CAN_EDIT_INVITE"},
+        ["CanBan"] = {Description="Allows the user to ban from their own faction."},
+        ["CanUnban"] = {Description="Allows the user to unban a member from their own faction."},
+        ["CanDisbandFaction"] = {Description="Allows a user to disband their faction."},
+        ["CanEditAll"] = {Description="Allows a user to edit any faction detail."},
+        ["CanEditDescription"] = {Description="Allows a user to edit the faction's description."},
+        ["CanEditName"] = {Description="Allows a user to edit the faction's name."},
+        ["CanEditColor"] = {Description="Allows a user to edit the faction's color."},
+        ["CanEditInvite"] = {Description="Allows a user to edit the faction's invite status."},
 
         --permssions
-        ["CanEditPermissions"] = {Description="Allows a user to edit a faction's permission structure.", Alias="CAN_EDIT_PERMISSIONS"},
-        ["CanAddPermissions"] = {Description="Allows a user to add a permission to a member.", Alias="CAN_ADD_PERMISSIONS"},
-        ["CanSetRanks"] = {Description="Allows a user to set a member's rank.", Alias="CAN_SET_RANKS"},
-        ["CanRemoveRanks"] = {Description="Allows a user to remove a member's rank.", Alias="CAN_REMOVE_RANKS"},
-        ["CanRemovePermissions"] = {Description="Allows a user to remove a member's permissions.", Alias="CAN_REMOVE_PERMISSIONS"},
+        ["CanEditPermissions"] = {Description="Allows a user to edit a faction's permission structure."},
+        ["CanAddPermissions"] = {Description="Allows a user to add a permission to a member."},
+        ["CanSetRanks"] = {Description="Allows a user to set a member's rank."},
+        ["CanRemoveRanks"] = {Description="Allows a user to remove a member's rank."},
+        ["CanRemovePermissions"] = {Description="Allows a user to remove a member's permissions."},
 
         --housekeeping
-        ["CanViewLogs"] = {Description="Allows a user to view the faction's logs.", Alias="CAN_VIEW_LOGS"},
-        ["CanViewFactionWiki"] = {Description="Allows a user to view their faction's wiki.", Alias="CAN_VIEW_FACTION_WIKI"},
-        ["CanEditFactionWiki"] = {Description="Allows a user to edit their faction's wiki.", Alias="CAN_EDIT_FACTION_WIKI"}, 
+        ["CanViewLogs"] = {Description="Allows a user to view the faction's logs."},
+        ["CanViewFactionWiki"] = {Description="Allows a user to view their faction's wiki."},
+        ["CanEditFactionWiki"] = {Description="Allows a user to edit their faction's wiki."}, 
 
         --misc
-        ["CanSendAllMessage"]= {Description="Allows a user to send a member to any faction member.", Alias="CAN_SEND_ALL_MESSAGE"},
-        ["CanReceiveAllMessage"] = {Description="Allows a user to receive a message from anyone.", Alias="CAN_RECEIVE_ALL_MESSAGE"},
-        ["CanSendFactionMessage"] = {Description="Allows a user to send a message to their own faction.", Alias="CAN_SEND_FACTION_MESSAGE"},
-        ["CanReceiveFactionMessage"] = {Description="Allows a user to receive a faction message from their own faction.", Alias="CAN_RECEIVE_FACTION_MESSAGE"},
-        ["CanSpawnXPObject"] = {Description="Allows a user to spawn a XP gathering object.", Alias="CAN_SPAWN_XP_OBJECT"},
-        ["CanDeleteXPObject"] = {Description="Allows a user to remove a XP gathering object.", Alias="CAN_DELETE_XP_OBJECT"},
-        ["CanDeclareWar"] = {Description="Allows a user to declare war on other factions.", Alias="CAN_DECLARE_WAR"},
-        ["CanUndeclareWar"] = {Description="Allows a user to remove a war from another faction.", Alias="CAN_UNDECLARE_WAR"},
-        ["CanSetAllies"] = {Description="Allows a user to set allies.", Alias="CAN_SET_ALLIES"},
-        ["CanSetEnemies"] = {Description="Allows a user to set enemies.", Alias="CAN_SET_ENEMIES"},
-        ["CanRemoveAllies"] = {Description="Allows a user to remove allies.", Alias="CAN_REMOVE_ALLIES"},
-        ["CanRemoveEnemies"] = {Description="Allows a user to remove enemies.", Alias="CAN_REMOVE_ENEMIES"},
-        ["CanHireMercs"] = {Description="Allows a user to hire mercenaries.", Alias="CAN_HIRE_MERCS"},
-        ["CanFireMercs"] ={Description="Allows a user to fire mercenaries.", Alias="CAN_FIRE_MERCS"},
-        ["CanSendInvite"] = {Description="Allows a user to send out faction invites.", Alias="CAN_SEND_INVITE"},
-        ["CanRevokeInvite"] = {Description="Allows a user to revoke a faction invite.", Alias="CAN_REVOKE_INVITE"}
+        ["CanSendAllMessage"]= {Description="Allows a user to send a member to any faction member."},
+        ["CanReceiveAllMessage"] = {Description="Allows a user to receive a message from anyone."},
+        ["CanSendFactionMessage"] = {Description="Allows a user to send a message to their own faction."},
+        ["CanReceiveFactionMessage"] = {Description="Allows a user to receive a faction message from their own faction."},
+        ["CanSpawnXPObject"] = {Description="Allows a user to spawn a XP gathering object."},
+        ["CanDeleteXPObject"] = {Description="Allows a user to remove a XP gathering object."},
+        ["CanDeclareWar"] = {Description="Allows a user to declare war on other factions."},
+        ["CanUndeclareWar"] = {Description="Allows a user to remove a war from another faction."},
+        ["CanSetAllies"] = {Description="Allows a user to set allies."},
+        ["CanSetEnemies"] = {Description="Allows a user to set enemies."},
+        ["CanRemoveAllies"] = {Description="Allows a user to remove allies."},
+        ["CanRemoveEnemies"] = {Description="Allows a user to remove enemies."},
+        ["CanHireMercs"] = {Description="Allows a user to hire mercenaries."},
+        ["CanFireMercs"] ={Description="Allows a user to fire mercenaries."},
+        ["CanSendInvite"] = {Description="Allows a user to send out faction invites."},
+        ["CanRevokeInvite"] = {Description="Allows a user to revoke a faction invite."}
 }
 
 --super special permissions not used by factions specifically
 fpm.Permissions.SpecialPermissions = {
-    ["IsDeveloper"] = {Description="Gives full permissions over everything that is cfc_Factions.", Alias="_IS_A_DEV_"},
-    ["IsTester"] = {Description="A test permission to let users access experimental features.", Alias="IS_A_TESTER"},
-    ["IsFactionsAdmin"] = {Description="Lets a user have full control over other factions. ", Alias="FACTIONS_ADMIN"},
-    ["CanCreateFaction"] = {Description="Lets a user create their own factions.", Alias="CAN_CREATE_FACTION"},
-    ["CanJoinFaction"] = {Description="Lets a user join other's factions.", Alias="CAN_JOIN_FACTION"},
-    ["AccessAll"] = {Description="Lets a user access factions and its content.", Alias="ACCESS_ALL"},
-    ["CanLeaveFaction"] = {Description="Lets a user leave their faction.", Alias="CAN_LEAVE_FACTION"},
-    ["TestPerm"] = {Description="Test permission, please ignore.", Alias="TEST_PERM"}
+    ["IsDeveloper"] = {Description="Gives full permissions over everything that is cfc_Factions."},
+    ["IsTester"] = {Description="A test permission to let users access experimental features."},
+    ["IsFactionsAdmin"] = {Description="Lets a user have full control over other factions. "},
+    ["CanCreateFaction"] = {Description="Lets a user create their own factions."},
+    ["CanJoinFaction"] = {Description="Lets a user join other's factions."},
+    ["AccessAll"] = {Description="Lets a user access factions and its content."},
+    ["CanLeaveFaction"] = {Description="Lets a user leave their faction."},
+    ["TestPerm"] = {Description="Test permission, please ignore."}
 }
 
-local function lookUpAlias(alias)
-    for k, v in pairs(fpm:FetchMergedPermissions()) do
-        if v.Alias == alias.Alias then return k end
-    end
-end
 
 --//interal ranks inside a self contained faction. These will always be avaible to default to encase
 --a user decides to mess up their internal ranks. 
@@ -222,16 +217,10 @@ function fpm:IsValidPermission(cmd)
             return true
         end
 
-        if v.Alias == string.lower(cmd) then
-            return true
-        end
     end
 
     for n, m in pairs(fpm.Permissions.SpecialPermissions) do
         if string.lower(n) == string.lower(cmd) then
-            return true
-        end
-        if m.Alias == string.lower(cmd) then
             return true
         end
     end
