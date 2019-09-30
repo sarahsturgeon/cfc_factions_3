409a0b3ed4ce4b50a121e7e3c920ccbd8ec37088
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-09-30 14:47:14 -0700

    Update lua/cfcfactions/core/cl/dermas/minis/cl_alertbox.lua

--- lua/cfcfactions/core/cl/dermas/minis/cl_alertbox.lua
+++ lua/cfcfactions/core/cl/dermas/minis/cl_alertbox.lua
@@ -22,7 +22,7 @@ function Panel:Init()
 
     self.ErrMsg = vgui.Create("DLabel", self.MiniPanel)
     self.ErrMsg:Dock( RIGHT )
-    self.ErrMsg:SetColor(msgtype and msgtype ~= nil or Color( 255,0,0) )
+    self.ErrMsg:SetColor( msgtype and msgtype ~= nil or Color( 255, 0, 0 ) )
     self.ErrMsg:SetText(msg and msg ~= nil or cfcFactions.ErrorMessages["general-error"])
 
     self.CreationTime = CurTime()
