d34c9cb3222882e533c2b4b67947416d32709a60
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-09-30 14:37:01 -0700

    Update lua/cfcfactions/core/sv/sv_init.lua

--- lua/cfcfactions/core/sv/sv_init.lua
+++ lua/cfcfactions/core/sv/sv_init.lua
@@ -88,7 +88,7 @@ local function cfcOnPlayerInitialSpawn( ply )
     --Always load a user as if never exsisted. Afterwards, load their proper data from source
     cfcFactions.Users:registeruser(ply, nil, nil)
 
-    for k, v in pairs(string.Explode("\n", cfcFactions:LoadNews( )) ) do
+    for k, v in pairs( string.Explode( "\n", cfcFactions:LoadNews() ) ) do
         net.Start( "CFC_Fac_SendNews" )
             net.WriteString(v .. "\n")
             net.WriteString( ply:Nick() )
