6c61542ca81a88679bbdc2d26b15ea18e7ac9d32
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-12 22:09:44 -0400

    Various whitespace changes

--- lua/cfcfactions/core/cl/dermas/cl_examplederma.lua
+++ lua/cfcfactions/core/cl/dermas/cl_examplederma.lua
@@ -1,18 +1,18 @@
---if not on client, lets not run this
+-- if not on client, lets not run this
 if not CLIENT then return end
 
---Our master panel table
+-- Our master panel table
 local Panel = {}
 
---Registers the panel to be loaded into the menubar if desired
---change tab_name and index to be desired. index being where you wish the tab to appear
+-- Registers the panel to be loaded into the menubar if desired
+-- change tab_name and index to be desired. index being where you wish the tab to appear
 
---cfcFactions:RegisterDermaMenu( tab_name, Panel, index )
+-- cfcFactions:RegisterDermaMenu( tab_name, Panel, index )
 
---main function to init the panel when it is called
+-- main function to init the panel when it is called
 function Panel:Init()
 
 end
 
---Needed to actually register this derma to be used
---vgui.Register( 'D_cfcexamplederma', Panel )
+-- Needed to actually register this derma to be used
+-- vgui.Register( 'D_cfcexamplederma', Panel )
--- lua/cfcfactions/core/cl/dermas/cl_usersderma.lua
+++ lua/cfcfactions/core/cl/dermas/cl_usersderma.lua
@@ -1,7 +1,7 @@
 if not CLIENT then return end
 
 local Panel = {}
---cfcFactions.Dermas["View Users"] = {1, Panel}
+-- cfcFactions.Dermas["View Users"] = {1, Panel}
 
 cfcFactions:RegisterDermaMenu( "View Users", Panel, 1 )
 
@@ -19,40 +19,40 @@ end
 
 vgui.Register( 'D_cfcusersderma', Panel )
 
---TODO: tie into the system
+-- TODO: tie into the system
 
 local function fetchFactionUsers()
-    
+
 end
 
 net.Receive( "CFC_Fac_FetchUsers", fetchFactionUsers )
 
 local function factionUserOffline()
-    
+
 end
 
 net.Receive( "CFC_Fac_UserOffline", factionUserOffline )
 
 local function factionFetchOnline()
-    
+
 end
 
 net.Receive( "CFC_Fac_FetchOnline", factionFetchOnline )
 
 local function factionUserChange()
-    
+
 end
 
 net.Receive( "CFC_Fac_UserChange", factionUserChange )
 
 local function factionUserUpdateStats()
-    
+
 end
 
 net.Receive( "CFC_Fac_UserUpdateStats", factionUserUpdateStats )
 
 local function factionUserDeleted()
-    
+
 end
 
 net.Receive( "CFC_Fac_UserDeleted", factionUserDeleted )
