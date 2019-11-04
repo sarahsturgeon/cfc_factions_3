68f205945404a478ad28432207ceb5323318301d
mw-voo <necro343@gmail.com>
2019-11-03 21:02:12 -0600

    Inital upload
    2 resources have been added with this PR

--- lua/cfcfactions/core/cl/dermas/cl_factionsderma.lua
+++ lua/cfcfactions/core/cl/dermas/cl_factionsderma.lua
@@ -34,7 +34,11 @@ function Panel:Init()
         end )
 
         menu:AddOption( "Edit", function()
-            --
+            self.EditMiniPanel = vgui.Create( "D_cfcfactionedit", self.MainContainer )
+            --Fill in details for LoadFactionDetails
+            --Get the ID of the current selected faction, fetch the id by the clientside table
+            --where all factions are being stored, and pass that table's proper values into the function
+            --self.EditMini:LoadFactionDetails( name, description, color, inviteonly )
         end )
 
         menu:AddOption( "Flag for Administration", function()
@@ -172,7 +176,18 @@ local function addFaction( tbl )
     local tmpLock = Faction.Invite and "L" or ""
     local PrettyOwnerName = player.GetBySteamID64( Faction.Owner ):Nick()
     -- Add faction to clientside table
-    cfcFactions.FactionsListView:AddLine( tmpLock, Faction.Name, Faction.Description, PrettyOwnerName, ( Faction.Kills .. "/" .. Faction.Deaths ), Faction.ID )
+    --resource/icons/lock_icon/lock_locked.png
+    --resource/icons/lock_icon/lock_unlocked.png
+    local TinyLock = vgui.Create( "DImage", cfcFactions.FactionsListView )
+    TinyLock:SetSize( 15, 15 )
+    TinyLock:CenterHorizontal( 0.5 )
+
+    if tmpLock then
+        TinyLock:SetImage( "resource/icons/lock_icon/lock_locked.png" )
+    else
+        TinyLock:SetImage( "resource/icons/lock_icon/lock_unlocked.png" )
+    end
+    cfcFactions.FactionsListView:AddLine( TinyLock, Faction.Name, Faction.Description, PrettyOwnerName, ( Faction.Kills .. "/" .. Faction.Deaths ), Faction.ID )
     cfcFactions.FactionsListView:DataLayout()
 
 end
