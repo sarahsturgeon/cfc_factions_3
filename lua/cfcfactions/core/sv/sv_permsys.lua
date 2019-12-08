f1f0110e001502938cb4ae105b01cb769e343623
mw-voo <voodoonecro@protonmail.com>
2019-12-07 19:15:39 -0600

    Inital commit of all things clientside
    Could of ether added a 'not' on it, or just make it return true

--- lua/cfcfactions/core/sv/sv_permsys.lua
+++ lua/cfcfactions/core/sv/sv_permsys.lua
@@ -191,7 +191,7 @@ function fpm:hasPermission( player, permission )
     end
     -- If they can't even access factions, just return false for everything.
     if table.HasValue( PlayerTable,  "AccessAll" ) then
-        return false
+        return true
     end
 
     -- Check the global CFC Permissions for the permission.
