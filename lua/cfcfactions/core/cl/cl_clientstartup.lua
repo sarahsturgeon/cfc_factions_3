bcafda5b984127fbb2d179c6200dca420a48645a
mw-voo <voodoonecro@protonmail.com>
2019-12-16 11:22:58 -0600

    Fixed old typos

--- lua/cfcfactions/core/cl/cl_clientstartup.lua
+++ lua/cfcfactions/core/cl/cl_clientstartup.lua
@@ -2,7 +2,6 @@
 function cfcFactions:DisplayMenu()
 
     if not self.MainMenu then
-        cfcFactions:InitColorSchemes()
         self.MainMenu = vgui.Create( "D_cfcmainderma" )
         self.MainMenu:SetVisible( false ) 
     end
