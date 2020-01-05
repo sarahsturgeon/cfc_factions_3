c297ebce1096e02a4372bec2926c2a0887acca18
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-01-05 05:12:15 -0500

    Fix violations in sh_userext.lua

--- lua/cfcfactions/core/sh/sh_userext.lua
+++ lua/cfcfactions/core/sh/sh_userext.lua
@@ -7,19 +7,21 @@ Purpose: Shared functions to fetch users from the global table cfcFactions.Users
 function cfcFactions:GetUser( ply )
     if SERVER then
         return cfcFactions.Users[ply:SteamID64()]
-    else
-        -- clientside get the user
     end
+
+    --TODO: Get user on clientside
 end
 
 -- old function, make sure to redo when this is called
 -- should return a table of your fellow factioneers ( of same id )
 function cfcFactions:GetFactionUsers( factionid )
     local tmp = {}
+
     for _, ply in pairs( cfcFactions.Users ) do
         if ply.FactionID == factionid then
             table.insert( tmp, ply )
         end
     end
+
     return tmp
 end
