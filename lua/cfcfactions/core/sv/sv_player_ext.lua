c36c8c2f8e9221cf488d8aa94cad91c651e82787
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-01-05 04:44:33 -0500

    Fix violations in sv_player_ext.lua

--- lua/cfcfactions/core/sv/sv_player_ext.lua
+++ lua/cfcfactions/core/sv/sv_player_ext.lua
@@ -15,7 +15,7 @@ function meta:CFCToggleMenu()
         cfcFactions:SendNotifcation( "factions-ban", mtype, player )
         return
     else
-        net.Start( 'CFC_Fac_ToggleDerma' )
+        net.Start( "CFC_Fac_ToggleDerma" )
         net.Send( self )
     end
 end
@@ -28,33 +28,29 @@ end
 function meta:IsInFaction()
     if self:IsMerc() == true then return true end
 
-    local userHasFaction = nil
-    if ( not ( self:GetFactionID() == nil ) ) then
-        userHasFaction = true
-    else
-        userHasFaction = false
-    end
+    local userHasFaction = self:GetFactionID() ~= nil
+
     return userHasFaction
 end
 
 -- Obtains the player's faction id, or 0 if not. Bots always return 'b0t'
 function meta:GetFactionID()
     if self:IsBot() then return "b0t" end
-    if self:IsPlayer() then
-        return fpm[self:SteamID64()]
-    end
+
+    if not self:IsPlayer() then return end
+
+    return fpm[self:SteamID64()]
 end
 
 -- Obtains the player's rank if in a faction. Returns empty string if not
 function meta:GetFactionRank()
-    if self:IsInFaction() then
-        return fpm[self:SteamID64()].FactionRank
-    else
-        return ""
-    end
+    if not self:IsInFaction() then return "" end
+
+    return fpm[self:SteamID64()].FactionRank
 end
 
 -- Gets a player's faction as a table. Returns an empty table if not in one
+-- TODO: Make these return values consistent
 function meta:GetFaction()
     if not self:IsPlayer() then return false end
     if not self:IsInFaction() then
