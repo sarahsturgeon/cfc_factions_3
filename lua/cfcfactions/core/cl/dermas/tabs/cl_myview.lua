19cf6fbfb16fbb379fb7936d1e83cf2dc42d4ad9
mw-voo <voodoonecro@protonmail.com>
2020-01-23 08:44:40 -0600

    Planning of MyView player view system

--- lua/cfcfactions/core/cl/dermas/tabs/cl_myview.lua
+++ lua/cfcfactions/core/cl/dermas/tabs/cl_myview.lua
@@ -2,6 +2,9 @@ local PANEL = {}
 local cfg = ColorSchemes
 vgui.Register( 'D_myview', PANEL )
 function PANEL:Init()
+    self.PlayerGroups = {}
+    self.GroupContainers = {}
+
     self.MainPanelView = vgui.Create( "DPanel" , self )
     self.MainPanelView:Dock( FILL )
     self.MainPanelView:SetBackgroundColor( cfg.InlinePanel )
@@ -44,4 +47,65 @@ function PANEL:Init()
     self.BottomPanel:SetBackgroundColor( cfg.InlinePanel )
 
     --Todo fill out bottom panel to include member groups / members in each group. see picture of viewfactionderma
+end
+--Handles drawing the vgui panels for Group and Players
+--A single container should contain a Panel, Panel, Group Name Label, Panel, Player panel for each player
+--[[
+
+    Panel
+    {
+        Panel
+        {
+            [GroupName]
+                Panel
+                {
+                    [Player 1] { Panel }
+                    [Player 2] { Panel }
+                    [Player 3] { Panel }
+                    [Player 4] { Panel }
+                }
+        },
+        Panel
+        {
+            [GroupName 2]
+                Panel
+                {
+                    [Player 1] { Panel }
+                    [Player 2] { Panel }
+                    [Player 3] { Panel }
+                    [Player 4] { Panel }
+                }
+        }        
+
+    }
+
+]]--
+function PANEL:DrawGroupContainer()
+    for Indx, Groups in self.PlayerGroups do
+        if table.Count( Groups ) > 0 then
+            self.GroupContainers[Groups] = vgui.Create( "DPanel", self.BottomPanel )
+            self.GroupContainers[Groups]:Dock( LEFT )
+        end
+    end
+end
+function PANEL:AddPlayer( ply, group )
+
+    --[[
+        Structure of self.PlayerGroups
+
+        ["Group_Name"] = { Players }
+
+        ["Owner"] = {"STEAMID9829381"},
+        ["Meatshields"] = {"STEAM90388111", "STEAM783091982"}
+
+    ]]--
+    for Indx, Groups in self.PlayerGroups do
+        if table.HasValue( Groups, ply ) then
+            if Groups ~= group then
+                --If the user exists already but the group is different than their current group, change their group and remove the old entry
+                self.PlayerGroups[group].ply = nil
+                table.Insert( self.PlayerGroups[group], ply )
+            end
+        end
+    end
 end
